c7fea790d15461a7ce31827b9505702c935022d5
Sweepyoface <github@sweepy.pw>
2016-05-17 20:06:38 -0400

    Better comment

--- lua/autorun/server/init_adminstick.lua
+++ lua/autorun/server/init_adminstick.lua
@@ -6,7 +6,7 @@ hook.Add("PlayerLoadout", "AdminStick_GiveTheStick", function(ply)
 	end
 end)
 
-hook.Add("PlayerSpawn", "AdminStick_GiveTheStick_Backup", function(ply) // Backup in case the PlayerLoadout hook is overwritten in a custom gamemode
+hook.Add("PlayerSpawn", "AdminStick_GiveTheStick_Backup", function(ply) -- Backup in case the PlayerLoadout hook is overwritten in a custom gamemode
 	if (StickConfig.ShouldGiveToPerson(ply)) and (!ply:HasWeapon("thc_adminstick")) then
 		ply:Give("thc_adminstick")
 	end
